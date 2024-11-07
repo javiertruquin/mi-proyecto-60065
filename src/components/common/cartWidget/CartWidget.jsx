9c018090239c825ba91c929ef80d5bb53fd42f8d
javiertruquin <javiertruquin@gmail.com>
2024-11-07 20:45:14 -0300

    CarritoWidget simplificado

--- src/components/common/cartWidget/CartWidget.jsx
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -2,18 +2,8 @@ import { useContext } from "react";
 import { CartContext } from "../../../context/CartContext";
 import { Link } from "react-router-dom";
 import Badge from "@mui/material/Badge";
-import { styled } from "@mui/material/styles";
 import IconButton from "@mui/material/IconButton";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-
-const StyledBadge = styled(Badge)(({ theme }) => ({
-    "& .MuiBadge-badge": {
-        right: -7,
-        top: 13,
-        border: `2px solid ${theme.palette.background.paper}`,
-        padding: "0 4px",
-    },
-}));
 
 export const CartWidget = () => {
     const { getTotalQuantityCart } = useContext(CartContext);
@@ -22,9 +12,9 @@ export const CartWidget = () => {
     return (
         <Link to="/cart">
             <IconButton sx={{ margin: "20px" }} aria-label="cart">
-                <StyledBadge badgeContent={total} color="secondary">
+                <Badge badgeContent={total} color="secondary" showZero>
                     <ShoppingCartIcon />
-                </StyledBadge>
+                </Badge>
             </IconButton>
         </Link>
     );
